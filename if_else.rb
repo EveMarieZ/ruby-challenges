786f4cf4f18d3c4a6acfbfd25671dd76fd9e7081
Queen Bavmorda <e.zaunbrecher@gmail.com>
2018-06-20 14:50:20 -0500

    Add if else

---
+++ if_else.rb
@@ -0,0 +1,27 @@
+if 1 + 1 == 2
+	puts "1 and 1 does indeed equal 2"
+end
+
+my_name = 'Eve'
+if my_name == 'Eve'
+	puts "Helloooooo, Eve!"
+else 
+	puts "Oops, I thought your name was Eve. Sorry about that, #{my_name}!"
+end
+
+fav_color = 'pink'
+if (fav_color =='red')
+	puts "Red like fire!"
+elsif (fav_color == 'orange')
+	puts "Orange like, well...an orange."
+elsif (fav_color == 'yellow')
+	puts "Yellow daffodils are so pretty in the spring."
+elsif (fav_color == 'green')
+	puts "Have you been to the Emerald City of Oz?"
+elsif (fav_color == 'blue')
+	puts "Blue like the sky!"
+elsif (fav_color == 'purple')
+	puts "Purple plums are the tastiest."
+else 
+	puts "Cool."
+end

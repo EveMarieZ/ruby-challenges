50e86a2653bc1f99733e76c76d5055435c9a3367
Queen Bavmorda <e.zaunbrecher@gmail.com>
2018-09-27 14:27:48 -0400

    case statements

---
+++ case.rb
@@ -0,0 +1,11 @@
+weather_today = 'sunny'
+
+when 'sunny'
+	puts "It's a beautiful sunny day! Ew."
+when 'rainy'
+	puts "It's raining out, yay!"
+when 'cloudy'
+	puts "There are lots of clouds today."
+when 'humid'
+	puts "It's humid as the jungle out there!"
+end

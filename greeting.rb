94aca287db07da50d509a96c028cc4b35529a6d1
Queen Bavmorda <e.zaunbrecher@gmail.com>
2018-10-08 15:18:23 -0400

    fixed time logic bug

--- greeting.rb
+++ greeting.rb
@@ -5,9 +5,9 @@ end
 
 def greeting(name)
 	current_hour = determine_current_hour
-	if(current_hour > 3 && current_hour < 12); time = "morning" 
-	elsif(current_hour > 12 && current_hour < 18); time = "afternoon" 
-	elsif(current_hour > 18 || current_hour < 2); time = "evening"
+	if(current_hour >= 3 && current_hour < 12); time = "morning" 
+	elsif(current_hour >= 12 && current_hour < 18); time = "afternoon" 
+	elsif(current_hour >= 18 || current_hour < 2); time = "evening"
 	end
 	puts "Good #{time}, #{name.capitalize}!"
 end
@@ -16,3 +16,5 @@ puts "Hello! What's your name?"
 name = gets
 
 greeting(name)
+
+
